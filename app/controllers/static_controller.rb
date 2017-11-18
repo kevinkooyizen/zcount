d347505bd2b9b35fe8fc5458228ad26e96104578
Koo Yi Zen <kooyizen@hotmail.com>
2017-11-18 17:23:57 +0800

    made front page dynamic

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,8 @@ class StaticController < ApplicationController
     @user = User.new
   end
   
+  def user_params
+    params.require(:user).permit(:monthly_income, :monthly_expense)
+  end
+  
 end
