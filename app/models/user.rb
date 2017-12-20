37b518af3ce4c08a897da8bff97fcc42a361f2e8
Koo Yi Zen <kooyizen@hotmail.com>
2017-12-21 05:27:37 +0800

    added socso

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,108 @@
 class User < ApplicationRecord
 	include ActionView::Helpers::NumberHelper
 
-	# Tax
+	# SOCSO
+	
+	def year_socso
+		0.005*self.year_income
+	end
+	
+	def month_socso
+		0.005*self.month_income
+	end
+	
+	def year_income_after_socso
+		self.year_income*(1-0.005)
+	end
+	
+	def month_income_after_socso
+		self.month_income*(1-0.005)
+	end
+	
+	def daily_income_after_socso
+		self.daily_income*(1-0.005)
+	end
+	
+	def hourly_income_after_socso
+		self.hourly_income*(1-0.005)
+	end
+
+	# EPF
+	
+	def year_epf
+		self.epf*self.year_income
+	end
+	
+	def month_epf
+		self.epf*self.month_income
+	end
+	
+	def year_income_after_epf
+		self.year_income*(1-self.epf)
+	end
+	
+	def month_income_after_epf
+		self.month_income*(1-self.epf)
+	end
+	
+	def daily_income_after_epf
+		self.daily_income*(1-self.epf)
+	end
+	
+	def hourly_income_after_epf
+		self.hourly_income*(1-self.epf)
+	end
+	
+	# EXPENSES
+	
+	def year_expense
+		self.month_expense*12
+	end
+	
+	def daily_expense
+		(self.month_expense)/30
+	end
+	
+	def hourly_expense
+		self.month_expense/(20*8)
+	end
+	
+	def net_month
+		self.month_income - self.month_expense
+	end
+	
+	# INCOME
+	def year_income
+		self.month_income*12
+	end
+	
+	def daily_income
+		(self.month_income)/30
+	end
+	
+	def hourly_income
+		self.month_income/(20*8)
+	end
+	
+	# NET
+	
+	def year_after_tax_epf_socso
+		if self.taxable?
+			self.year_income - self.year_total_tax - self.year_epf - self.year_socso
+		else
+			self.year_income - self.year_epf - self.year_socso
+		end
+	end
+	
+	def month_after_tax_epf_socso
+		if self.taxable?
+			self.month_income - self.month_total_tax - self.month_epf - self.month_socso
+		else
+			self.month_income - self.month_epf - self.month_socso
+		end
+	end
+	
+	# TAX
 	
 	def taxable?
 		if self.month_income > 3000
@@ -31,69 +132,12 @@ class User < ApplicationRecord
 		self.year_tax_20/12
 	end
 	
-	def total_year_tax
+	def year_total_tax
 		self.year_tax_10 + self.year_tax_20
 	end
 	
-	def total_month_tax
+	def month_total_tax
 		self.month_tax_10 + self.month_tax_20
 	end
-	
-	# Gross
-	def year_income
-		self.month_income*12
-	end
-	
-	def daily_income
-		(self.month_income)/30
-	end
-	
-	def hourly_income
-		self.month_income/(20*8)
-	end
 
-	# EPF
-	
-	def year_epf
-		self.epf*self.year_income
-	end
-	
-	def month_epf
-		self.epf*self.month_income
-	end
-	
-	def year_income_after_epf
-		self.year_income*(1-self.epf)
-	end
-	
-	def month_income_after_epf
-		self.month_income*(1-self.epf)
-	end
-	
-	def daily_income_after_epf
-		self.daily_income*(1-self.epf)
-	end
-	
-	def hourly_income_after_epf
-		self.hourly_income*(1-self.epf)
-	end
-	
-	# Expenses
-	
-	def year_expense
-		self.month_expense*12
-	end
-	
-	def daily_expense
-		(self.month_expense)/30
-	end
-	
-	def hourly_expense
-		self.month_expense/(20*8)
-	end
-	
-	def net_month
-		self.month_income - self.month_expense
-	end
-	
 end
